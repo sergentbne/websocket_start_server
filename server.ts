f58aec0067af911c4dfc4efdba9fc9ec74eeb83f
louis Turpault <louis.turpault@gmail.com>
2025-09-23 11:13:44 -0400

    better console log

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ import WebSocket from 'ws';
 // Create a WebSocket server on port 8080
 const wss = new WebSocketServer({ port: 8080 });
 
-console.log('WebSocket server is running on ws://localhost:8080');
+console.log('WebSocket server is running on ws://0.0.0.0:8080');
 
 // Connection event handler
 wss.on('connection', (ws: WebSocket) => {
