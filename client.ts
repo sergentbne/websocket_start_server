17c9a663805eba863ec147f37449c5f38dc9731b
louis Turpault <louis.turpault@gmail.com>
2025-09-24 12:16:58 -0400

    asd

--- client.ts
+++ client.ts
@@ -1,21 +1,29 @@
 import WebSocket from 'ws';
 import {DataGetter} from "./GetData.ts";
-import 'ora';
-import ora from 'ora';
 
 
 // Create a WebSocket server on port 8080
 const wss = new WebSocket("ws://206.167.46.66:8080");
-const the_file = new DataGetter("minimally_compressible_file.bin");
+const the_file = new DataGetter("hello.txt");
 const file_data = await the_file.base64();
+const user_in = await get_user_input()
 
 wss.on('error', console.error);
 
 console.log('Websocket connected ws://localhost:8080');
 wss.on("open", () => {
-    const spinner = ora('sending big file').start();
-    wss.send(JSON.stringify({type: "big_data", data: file_data}));
-    spinner.stop();
+    switch (user_in) {
+        case "1": {
+            wss.send(JSON.stringify({type: "big_data", data: file_data}));
+            break;
+        }
+        case "2": {
+            wss.send(JSON.stringify({type: "get_data"}));
+            break;
+        }
+
+    }
+
 })
 
 // Connection event handler
@@ -23,3 +31,7 @@ wss.on("message", (message: WebSocket.Data) => {
     console.log(message.toString());
 });
 
+async function get_user_input() {
+    const user_in = prompt("what do you want to do: \n(1): send a big file (test)\n(2) get the data")
+    return user_in
+}
--- server.ts
+++ server.ts
@@ -22,13 +22,18 @@ wss.on('connection', (ws: WebSocket) => {
         const file_data = JSON.parse(data)
         switch (file_data.type) {
             case ("big_data"): {
-                let time = performance.now();
                 save_file(file_data.data);
                 console.log("file saved in %f", (performance.now() - time).toFixed(3))
                 break;
             }
             case ("get_data"): {
-
+                let array_of_numb = new Array(5).fill(0);
+                for (let i: number = 0; i < 5; i++) {
+                    array_of_numb[i] = Math.floor(Math.random() * 100);
+                }
+                data
+                let json_data = {"type": "return_value", "ultrasound": array_of_numb, "data": get_data_of_file()};
+                ws.send(JSON.stringify(json_data));
                 break;
             }
         }
@@ -54,6 +59,22 @@ async function save_file(file_data: string) {
     };
     // const json_data = JSON.parse(file_data);
     const raw_data = Buffer.from(file_data, "base64");
-    await Bun.write("alternative_data", raw_data, );
+    await Bun.write("alternative_data", raw_data);
 }
+
+function get_data_of_file() {
+    let data;
+    let file_test = Bun.file("alternative_data");
+
+    file_test.exists().then((value) => {
+        if (value){
+            Bun.file("./alternative_data").bytes().then((value) => {
+        data = value
+        return data
+        })
+    } else {
+        return "No Data"
+    }})
+        
+} 
 
